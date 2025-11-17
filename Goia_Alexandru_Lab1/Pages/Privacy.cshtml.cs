ab0ae9d582e0e9ff2af3212b570aa40ce172a3c3
adm1goia <adm1goia@REMOTEASSIST00>
2025-11-17 09:17:21 +0200

    Add project files.

---
+++ Goia_Alexandru_Lab1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Goia_Alexandru_Lab1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
